8fa6f3e968ee264da15929a43ca261f2fedb890c
ShulhaPavel <shulga.pavlo98@gmail.com>
2024-10-07 19:43:39 +0300

    Add task1.js file

---
+++ jsBasics/task1.js
@@ -0,0 +1,15 @@
+const name = "Pavel";
+const age = "26";
+const isHuman = true;
+const empty = null;
+let a;
+const mySymbol = Symbol('B');
+const BigIntNumber = 123124353453452n;
+
+console.log(name);
+console.log(age);
+console.log(isHuman);
+console.log(empty);
+console.log(a);
+console.log(mySymbol);
+console.log(BigIntNumber);
