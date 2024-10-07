a29a1e969cd144e372f3e03f05acb6ed5548bfd4
ShulhaPavel <shulga.pavlo98@gmail.com>
2024-10-07 21:34:52 +0300

    Added a task2.js file

---
+++ jsBasics/task2.js
@@ -0,0 +1,6 @@
+let worker1 = "John Snow"
+let worker2 = "Tirion Lanister"
+console.log("Congratilation to our new co-workers: " + worker1 + " " + "and" + " " + worker2); //Конкатенація рядків
+
+let greeting = `Congratilation to our new co-workers: ${worker1} and ${worker2}`;
+console.log(greeting); //Шаблонні рядки
