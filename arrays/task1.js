8b93dea347cde63530246d4fe496a5d3bb182724
ShulhaPavel <shulga.pavlo98@gmail.com>
2024-10-23 19:36:53 +0300

    Added task3 file and updated task1 file

--- arrays/task1.js
+++ arrays/task1.js
@@ -1,5 +1,4 @@
-
-const numbers = [2, -5, 0, 7, -3, 0, 10, -8]
+export const numbers = [2, -5, 0, 7, -3, 0, 10, -8]
 let positiveCount = 0;
 let negativeCount = 0;
 let zeroCount = 0;
---
+++ arrays/task3.js
@@ -0,0 +1,4 @@
+import {numbers} from "./task1.js";
+
+const res = numbers.reduce((a, b) => a + b);
+console.log(res);
