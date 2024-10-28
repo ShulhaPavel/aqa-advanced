c01dc5c7eda25a50b58270e74816eb689990397d
ShulhaPavel <shulga.pavlo98@gmail.com>
2024-10-28 08:44:35 +0200

    Added file1

---
+++ objects/task1.js
@@ -0,0 +1,10 @@
+const student = {
+
+}
+
+student.firstName = "Oleg";
+student.lastName = "Sidorov";
+student.age = 15
+student.courses = ["Математика", "Історія", "Програмування"];
+
+console.log(student)
