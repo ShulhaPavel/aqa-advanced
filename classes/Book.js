0a14a1c1c252f4d9147376c252467783448423c2
ShulhaPavel <shulga.pavlo98@gmail.com>
2024-10-31 18:39:02 +0200

    Added Book file

---
+++ classes/Book.js
@@ -0,0 +1,18 @@
+class Book {
+    constructor(name, author, year){
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    };
+    printInfo(){
+        console.log(`Назва книги: ${this.name}; Автор:  ${this.author} ; Рік:  ${this.year} `)
+    }
+}
+
+const book1 = new Book("Дюна", "Фрэнк Герберт", 1965);
+const book2 = new Book("Ім'я троянди", "Умберто Еко", 1980);
+const book3 = new Book("Коротка історія майже всього на світі", "Білл Брайсон", 2003);
+
+book1.printInfo()
+book2.printInfo()
+book3.printInfo()
