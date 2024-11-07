70f9e0d79caf055ac9dece84edbf0b892f5b4274
ShulhaPavel <shulga.pavlo98@gmail.com>
2024-11-07 23:57:31 +0200

    added a new.test.js file

---
+++ new.test.js
@@ -0,0 +1,69 @@
+const axios = require('axios')
+
+
+describe('JSONPlaceholder', () => {
+
+    test("Get correct API test", async () => {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1', { 
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        console.log(response.data);
+        expect(response.status).toEqual(200); 
+    })
+
+    test("Get incorrect API test", async () => {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/post/1', { 
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        console.log(response.data);
+        expect(response.status).toEqual(200); 
+    })
+
+    test("Get incorrect status code", async () => {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/post/1', {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        console.log(response.data);
+        expect(response.status).toEqual(201); 
+    })
+
+    test("Create new post successfully", async () => {
+        const response = await axios.post('https://jsonplaceholder.typicode.com/posts', 
+        {
+            "title": "new title",
+            "body": "There is a new description in test",
+            "userId": 222,
+        },
+        {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        console.log(response.data);
+        expect(response.status).toEqual(201); 
+    })
+
+    test("Create new inccorect post", async () => {
+        const response = await axios.post('https://jsonplaceholder.typicode.com/posts', 
+        {
+            "title": "new title",
+            "body": "There is a new description in test",
+            "userId": 222,
+        },
+        {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        console.log(response.data);
+        expect(response.status).toEqual(200); 
+    })
+
+})
+
